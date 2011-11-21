d2cf4cb212708f756467147baf3eefeab4b53c31
Zhang Cheng <StephenPCG@gmail.com>
2011-11-21 16:06:55 +0800

    Imported Debian version 2011.08.23

---
+++ debian/geninfo.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "RELEASE=11.12"
+echo "CODENAME=oneiric"
+PLATFORM=`uname -i`
+echo "EDITION=\"Gnome $PLATFORM\""
+echo "DESCRIPTION=\"Linux Deepin 11.12 Develop branch\""
+echo "DESKTOP=Gnome"
+echo "TOOLKIT=GTK"
+echo "NEW_FEATURES_URL=http://www.linuxdeepin.com/start"
+echo "RELEASE_NOTES_URL=http://www.linuxdeepin.com/start"
+echo "USER_GUIDE_URL=http://www.linuxdeepin.com/help"
+echo "GRUB_TITLE=Linux Deepin 11.12 $PLATFORM"
